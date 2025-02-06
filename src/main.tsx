14397ac184701bab6a3dcab84b552e8c5c7329fa
RahulBenzeen <rahulbhardwaj@benzeenautoparts.com>
2025-02-06 17:41:41 +0530

    code updated

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,12 @@ import App from './App.tsx'
 import { store } from './store/store.ts'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './sw.ts'
-// import { I18nextProvider } from 'react-i18next'
-// import i18n from './i18n/config.ts'
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-      {/* <I18nextProvider i18n={i18n}> */}
-        <GoogleOAuthProvider clientId='704525634820-91rpge5c2osvqe099t43akf4i06ntsd5.apps.googleusercontent.com' >
+        <GoogleOAuthProvider clientId={`${import.meta.env.VITE_APP_GOOGLE_CLIENT_ID}`} >
           <App />   
         </GoogleOAuthProvider>
-      {/* </I18nextProvider> */}
     </Provider>
 )
 
